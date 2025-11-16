29cd42d5ba6a0e173d7fe6e17d53a5fffd7d5d83
j-pap-gh <john.papapostolou@gmail.com>
2025-11-16 16:54:18 +0100

    Update db_models.py

--- src/pass_inc/db_models.py
+++ src/pass_inc/db_models.py
@@ -1,10 +1,11 @@
 from datetime import date
 from typing import Optional
 
-from sqlalchemy import Date, Enum, Float, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Date, Enum, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .db import Base
+from .db_user_models import UserDB
 from .models import IncomeType
 
 
@@ -19,3 +20,7 @@ class IncomeStreamDB(Base):
     currency: Mapped[str] = mapped_column(String(8), nullable=False, default="USD")
     start_date: Mapped[Optional[date]] = mapped_column(Date, nullable=True)
     notes: Mapped[Optional[str]] = mapped_column(String(1024), nullable=True)
+
+    # new: link to User
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    owner: Mapped[UserDB] = relationship("UserDB", back_populates="income_streams")
